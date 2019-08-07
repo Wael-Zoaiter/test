9bc042508ef8878e93edb606109b5fb987bbe9b2
wael-zoaiter <wael.syr.95@gmail.com>
2019-08-07 11:00:47 +0300

    Updates

---
+++ precache-manifest.85b606ee9dbc742418fc03359acc9f5d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d491f4fe6b011507ca3d67e05d21e5",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "c95dfb5cfab12f63f6dc",
+    "url": "/test/static/js/2.d5038f69.chunk.js"
+  },
+  {
+    "revision": "f9ba2bc779b61eb355a8",
+    "url": "/test/static/js/main.610c56c4.chunk.js"
+  },
+  {
+    "revision": "2160d828785e96a67ccd",
+    "url": "/test/static/js/runtime~main.eded8796.js"
+  }
+]);
